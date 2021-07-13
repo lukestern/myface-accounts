4bfdc57b0fa1bd0d3705e95fd6d6f238afedba5d
Luke Stern <luke.stern@softwire.com>
2021-07-13 13:43:28 +0100

    WIP

--- myface-api/MyFace/Controllers/LoginController.cs
+++ myface-api/MyFace/Controllers/LoginController.cs
@@ -1,7 +1,24 @@
 ﻿namespace MyFace.Controllers
 {
-    public class LoginController
+        [ApiController]
+        [Route("/login")]
+    public class LoginController : ControllerBase
     {
+       
+            private readonly IUsersRepo _users;
 
-    }
+            public UsersController(IUsersRepo users)
+            {
+                _users = users;
+            }
+
+            [HttpGet("")]
+            public ActionResult<UserListResponse> Search([FromQuery] UserSearchRequest searchRequest)
+            {
+                var users = _users.Search(searchRequest);
+                var userCount = _users.Count(searchRequest);
+                return UserListResponse.Create(searchRequest, users, userCount);
+            }
+        
+        }
 }
